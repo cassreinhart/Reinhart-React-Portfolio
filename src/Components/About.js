2b8a40ad6d21a9b10014e40555b2af5aeedc9283
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-01-29 21:39:43 -0600

    added bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,7 @@ const About = () => {
 
   return (
     <div className='about'>
+        <p className='bio'>A Full Stack Developer, but more front-end leaning. What fulfills me is making a vision into a reality. I aim to grow my front-end skills continuously, particularly in responsive design, as well as become a JavaScript master.</p>
         <h2>Technical Skills</h2>
         <ul className='skills-list list'>
             {skills.map(s => <li key={s.id}>{s.name}</li>)}
